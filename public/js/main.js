5cf5c31846f6b0d425b2d627d6c09b568b2cf24b
Alexey Lukin <alukin@qarea.us>
2014-10-17 21:45:09 -0400

    init

---
+++ public/js/main.js
@@ -0,0 +1,1 @@
+$.getJSON('//www.test2.me:9091/api/users', function(data){ console.log(data) })
---
+++ server.js
@@ -0,0 +1,50 @@
+var h5bp = require('h5bp');
+var app1 = h5bp.createServer({ root: __dirname + '/public' });
+app1.listen(9090);
+
+
+// server.js
+var express    = require('express');
+var bodyParser = require('body-parser');
+var vhost      = require('vhost')
+
+var rapp = express();
+rapp.use(bodyParser.urlencoded({ extended: true }));
+rapp.use(bodyParser.json());
+rapp.use(express.static('public'));
+
+//routes
+var router = express.Router();
+router.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });
+});
+var users = [
+	{ id: 1, username: 'John Doe', age: 18 },
+	{ id: 2, username: 'Julianna Doel', age: 20 },
+	{ id: 3, username: 'Joshua Doely', age: 21 },
+	{ id: 4, username: 'Jerald Dougler', age: 23 },
+	{ id: 5, username: 'Jeronimo Doegerthy', age: 60 }
+]
+router.get('/users', function(req, res) {
+	res.json(users);
+});
+rapp.use('/api', router);
+
+var app = express();
+app.use(vhost('test1.me', rapp));
+app.use(vhost('www.test1.me', rapp));
+app.use(vhost('static.test1.me', rapp));
+app.use(vhost('test2.me', rapp));
+app.use(vhost('www.test2.me', rapp));
+
+var cors = function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+}
+cors = function(req, res, next){ next() }
+app.all('*', cors);
+
+var port = process.env.PORT || 9091;
+app.listen(port);
+console.log('Magic happens on port ' + port);
