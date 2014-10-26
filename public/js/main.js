a04397fab48a9744429e23d55b8c4f7209c78e60
Lukin Alexey <llamerr@gmail.com>
2014-10-25 22:36:16 -0400

    some changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,30 +1,40 @@
-var API_URL = '//test2.me:9091/api/';
-var usersControllerUrl = 'users';
-var ul = null;
-function loadUsers(){
-    console.log('loadUsers');
-    $.ajax({
-        url: API_URL + usersControllerUrl, 
-        success: function(data){
-            ul = $('#content ul');
-            if (ul.length == 0) {
-                ul = $('<ul></ul>');
-                console.log(ul)
-                $('#content').append(ul);
-            }            
-            data.map(function(el){
-                var li = document.createElement('li');
-                $(li).click(function(ev){
-                    console.log(ev, this);
-                    var self = this;
-                    $(this).hide(1000,function(){
-                        $(self).remove();    
-                    });
-                }).html(el.username)                
-                ul.append(li)
-            })            
+$( document ).ready(function(){
+    var $select = $('select');
+    var API_URL = 'http://localhost:3000/api/beers';
+    function setData(data) {
+        $('span').html(data._id);
+        for (var k in data) {
+            $('#'+k).val(data[k]);
         }
+    }
+    $select.change(function(ev){
+        var data = $( "select option:selected" ).data();
+        setData(data);
+    });
+    $('[type=submit]').click(function(e){
+        e.preventDefault();
+        var data = $( "select option:selected" ).data();
+        for (var k in data) {             
+            var v = $('#'+k).val();
+            if (typeof v != 'undefined') data[k] = v;
+        }
+        $.ajax({
+            type: "PUT",
+            url: API_URL + '/'+data._id,
+            data: data
+          });
     })
-}
-$('#loadData').click(loadUsers);
-
+    $.getJSON(API_URL,function(data){
+        for (var i = 0; i < data.length; i++) {
+            var match = data[i].name.match(/c[\.,o]+l/i);
+            if (match) continue;
+            var $o = $('<option>').html(data[i].name);
+            for (var k in data[i]) {              
+                $o.data(k, data[i][k]);
+            }
+            $select.append($o);
+        }
+        $select.change();
+    })
+    
+})
